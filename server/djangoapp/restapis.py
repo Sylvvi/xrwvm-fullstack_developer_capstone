ee384f80b9b6523ab69d00d6a6cec1ae977fa7a2
Sylvvi <silvivojk@gmail.com>
2024-09-19 14:48:00 -0400

    changes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,15 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
-
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url', 
+    default="http://localhost:5050/"
+)
 
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
         for key, value in kwargs.items():
-            params += key + "=" + value + "&"
-    request_url = backend_url + endpoint + "?" + params
+            params += f"{key}={value}&"
+    request_url = f"{backend_url}{endpoint}?{params}"
     print("GET from {}".format(request_url))
     try:
         # Call the get method of requests library with URL and parameters
@@ -26,9 +28,8 @@ def get_request(endpoint, **kwargs):
         print(f"Network exception occurred: {e}")
         return None
 
-
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = f"{sentiment_analyzer_url}analyze/{text}"
     try:
         # Call the get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -40,9 +41,8 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
         return None
 
-
 def post_review(data_dict):
-    request_url = backend_url + "/insert_review"
+    request_url = f"{backend_url}/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,11 +7,14 @@ import logging
 
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import (
+    get_request,
+    analyze_review_sentiments,
+    post_review
+)
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
 
 @csrf_exempt
 def login_user(request):
@@ -34,7 +37,6 @@ def login_user(request):
         return JsonResponse(response_data)
     return JsonResponse({"status": "Invalid request method"}, status=405)
 
-
 @csrf_exempt
 def logout_request(request):
     if request.method == 'POST':
@@ -46,7 +48,6 @@ def logout_request(request):
         }
         return JsonResponse(response_data)
     return JsonResponse({"status": "Invalid request method"}, status=405)
-
 
 @csrf_exempt
 def registration(request):
@@ -87,7 +88,6 @@ def registration(request):
         return JsonResponse(data)
     return JsonResponse({"status": "Invalid request method"}, status=405)
 
-
 def get_cars(request):
     count = CarMake.objects.filter().count()
     if count == 0:
@@ -102,7 +102,6 @@ def get_cars(request):
     ]
     return JsonResponse({"CarModels": cars})
 
-
 def get_dealerships(request, state="All"):
     endpoint = "/fetchDealers" if state == "All" else f"/fetchDealers/{state}"
     dealerships = get_request(endpoint)
@@ -110,7 +109,6 @@ def get_dealerships(request, state="All"):
         "status": 200,
         "dealers": dealerships
     })
-
 
 def get_dealer_details(request, dealer_id):
     if dealer_id:
@@ -121,7 +119,6 @@ def get_dealer_details(request, dealer_id):
             "dealer": dealership
         })
     return JsonResponse({"status": 400, "message": "Bad Request"})
-
 
 def get_dealer_reviews(request, dealer_id):
     if dealer_id:
@@ -135,7 +132,6 @@ def get_dealer_reviews(request, dealer_id):
             "reviews": reviews
         })
     return JsonResponse({"status": 400, "message": "Bad Request"})
-
 
 @csrf_exempt
 def add_review(request):
